c63d7016b870a3e438b9b29797d13b4f19546f0e
shivaravula1 <shivanayudu123@gmail.com>
2020-05-01 17:16:34 +0530

    Updates

---
+++ precache-manifest.05fc5c1969200657f806b5cea7b49efa.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea27cb22d6cc1a361a7a8db62e595f24",
+    "url": "/VibaHack/index.html"
+  },
+  {
+    "revision": "2f430bf14c2cf24b2580",
+    "url": "/VibaHack/static/css/main.126bfb88.chunk.css"
+  },
+  {
+    "revision": "8ffd0dce6aa8725f46c8",
+    "url": "/VibaHack/static/js/2.4e227861.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/VibaHack/static/js/2.4e227861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f430bf14c2cf24b2580",
+    "url": "/VibaHack/static/js/main.75ccbab4.chunk.js"
+  },
+  {
+    "revision": "98bdb5598932b4ea957a",
+    "url": "/VibaHack/static/js/runtime-main.553fe78c.js"
+  },
+  {
+    "revision": "7b3d0b2cee55bc9c7249596432fabb98",
+    "url": "/VibaHack/static/media/1.7b3d0b2c.jpeg"
+  },
+  {
+    "revision": "71c015e63d63193aab4334b6f262e5ed",
+    "url": "/VibaHack/static/media/10.71c015e6.png"
+  },
+  {
+    "revision": "eb10ec891adb51f0f17a3253654b5b42",
+    "url": "/VibaHack/static/media/11.eb10ec89.png"
+  },
+  {
+    "revision": "76271b3cd1222e3e7c4a522745166a75",
+    "url": "/VibaHack/static/media/2.76271b3c.png"
+  },
+  {
+    "revision": "d5604f479b10c54503b528e82aca2ad4",
+    "url": "/VibaHack/static/media/3.d5604f47.png"
+  },
+  {
+    "revision": "eddc482053b52aeb490a615bd481b256",
+    "url": "/VibaHack/static/media/4.eddc4820.jpg"
+  },
+  {
+    "revision": "7c6a105023c40723d6ea56a98ffdc4b1",
+    "url": "/VibaHack/static/media/45.7c6a1050.jpeg"
+  },
+  {
+    "revision": "c9b073de90739f5f483243e241383603",
+    "url": "/VibaHack/static/media/5.c9b073de.PNG"
+  },
+  {
+    "revision": "9d2f89b8d442643921a377daa9c3218e",
+    "url": "/VibaHack/static/media/6.9d2f89b8.jpg"
+  },
+  {
+    "revision": "9f357232157a312db07ae1bfbf1769ab",
+    "url": "/VibaHack/static/media/fullLogo.9f357232.png"
+  },
+  {
+    "revision": "55572dc2d52b665ec6cf6be942521633",
+    "url": "/VibaHack/static/media/logo.55572dc2.png"
+  }
+]);
